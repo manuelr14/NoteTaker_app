9efa68b5f88632b23d4d3dce20e1313395b49e97
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-02-21 00:33:26 -0500

    adding

---
+++ public/assets/js/notesData.js
@@ -0,0 +1,6 @@
+
+var notes = [];
+
+
+
+module.exports = notes;
--- public/server.js
+++ public/server.js
@@ -5,7 +5,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes=[ ];
+// const notes=[ "love", "yes"];
+
+var notes = require("./assets/js/notesData.js");
+
 app.get("/", (req, res) => {
     res.send( {message: 'hello'})
 
@@ -27,14 +30,28 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes",(req,res)=>{
     console.log("recieved note");
     console.log(req.body);
-    let newNote = req.body;
+    // let newNote = req.body;
+    for (var i = 0 ; i < notes.length; i++){
+
+        notes.push(req.body);
+        res.json(true);
+    }
+    // }else{
+    //     console.log("complete");
+    // }
     
-    saveNote(newNote);
-    
-    res.end("note created");
+});
+
+
+app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    notes.length = 0;
     
 
-});
+    res.json({ ok: true });
+  });
+// };
+
 
 // module.exports= start
 
